11ffa99a5da7396c10ceaf36404830c7c392ea8f
22920132203749 <438347829@qq.com>
2015-06-03 16:21:52 +0800

    Test

---
+++ my_stack.c
@@ -0,0 +1,127 @@
+/*
+ * my_stack的实现文件
+ * 如果采用C++的实现，那也可以把此文件名改为.cpp
+ * 但是记住g++才是编译C++的编译器
+ *
+ */
+
+#include "include/my_stack.h"
+int  stack[100];
+int top=0; 
+
+void stack_push(int x)
+{
+stack[top++]=x;
+}
+int stack_pop()
+{
+return stack[--top];
+}
+
+int stack_capacity()
+{
+return 100;
+}
+
+int stack_size()
+{
+return top; 
+}
+
+int stack_is_empty()
+{
+if (top==0) return 1;
+else return 0;
+}
+int stack_is_full();
+{
+if(top>=100) return 1;
+else return 0;
+}
+
+nclude "include/my_stack..h"
+int  stack[100];
+int top=0; 
+
+void stack_push(int x)
+{
+stack[top++]=x;
+}
+int stack_pop()
+{
+return stack[--top];
+}
+
+int stack_capacity()
+{
+return 100;
+}
+
+int stack_size()
+{
+return top; 
+}
+
+int stack_is_empty()
+{
+if (top==0) return 1;
+else return 0;
+}
+int stack_is_full();
+{
+if(top>=100) return 1;
+else return 0;
+
+}
+
+
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+}
+int  stack[100];
+int top=0; 
+
+void stack_push(int x)
+{
+stack[top++]=x;
+}
+int stack_pop()
+{
+return stack[--top];
+}
+
+int stack_capacity()
+{
+return 100;
+}
+
+int stack_size()
+{
+return top; 
+}
+
+int stack_is_empty()
+{
+if (top==0) return 1;
+else return 0;
+}
+int stack_is_full();
+{
+if(top>=100) return 1;
+else return 0;
+}
